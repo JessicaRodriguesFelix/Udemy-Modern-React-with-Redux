d40d6e373ab17c044beea67fccbedeeba155f3d6
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2024-02-07 17:03:53 -0500

    starts section 10, project initial setup, children props, button component

---
+++ comps/README.md
@@ -0,0 +1,4 @@
+# Section 10: Custom Navigating and Routing Systems
+
+- Enforcing a very consistent set of styling rules accross our entire application.
+- It is very important for companies that the fook and feel of applications you work on is xtremely consistent.
---
+++ comps/src/App.js
@@ -0,0 +1,20 @@
+import Button from "./Button";
+function App() {
+    return (
+      <div>
+        <div>
+          <Button text={'Click here!'}>Child</Button>
+        </div>
+        <div>
+          <Button>See Deal</Button>
+        </div>
+        <div>
+          <Button>Buy Now</Button>
+        </div>
+        <div>
+          <Button>Hide Ads</Button>
+        </div>
+      </div>
+    );
+}
+export default App;
