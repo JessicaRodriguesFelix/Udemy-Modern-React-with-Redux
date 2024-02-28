2db60563e9d3494ee3a91834fb531e27c97e3f33
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2024-02-28 15:42:09 -0500

    creates sidebar menu, adds link component, final touches

--- comps/src/components/Link.js
+++ comps/src/components/Link.js
@@ -1,25 +1,31 @@
 import useNavigation from '../hooks/use-navigation';
 import classNames from 'classnames';
 
-function Link({ to, children }) {
+function Link({ to, children, className, activeClassName }) {
+  const { navigate, currentPath } = useNavigation();
 
-const { navigate } = useNavigation();
+  const classes = classNames(
+    "text-blue-500",
+    className,
+    // adds the highlight in the nav bar
+    currentPath === to && activeClassName
+  );
 
-const classes = classNames('text-blue-500');
-
-const handleClick = (event) => {
+  const handleClick = (event) => {
     //allow users to open tab using keyboard shortcuts
     if (event.metaKey || event.ctrlKey) {
-        return;
+      return;
     }
     //stops the total page refresh
     event.preventDefault();
     navigate(to);
+  };
 
-}
-
-    return <a className={classes} href={to} onClick={ handleClick }>{children}</a>;
-
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
