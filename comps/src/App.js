21d5cb7ff56986dd1af1539e6a014b855c0aa1b9
JessicaRodriguesFelix <jessicarodrigues_felix@hotmail.com>
2024-02-29 20:14:09 -0500

    draft modal component and modalpage

--- comps/src/App.js
+++ comps/src/App.js
@@ -3,13 +3,14 @@ import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
 import ButtonPage from './pages/ButtonPage';
 import SideBar from './components/SideBar';
+import ModalPage from './pages/ModalPage';
 
 function App() {
 
 return (
-  <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+  <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
     <SideBar />
-    <div className='col-span-5'>
+    <div className="col-span-5">
       <Route path="/">
         <DropdownPage />
       </Route>
@@ -18,6 +19,9 @@ return (
       </Route>
       <Route path="/button">
         <ButtonPage />
+      </Route>
+      <Route path="/modal">
+        <ModalPage />
       </Route>
     </div>
   </div>
--- comps/src/components/SideBar.js
+++ comps/src/components/SideBar.js
@@ -3,11 +3,11 @@ import Link from './Link';
 function SideBar() {
 
     const links = [
-        { label: 'Dropdown', path: '/'},
-        { label: 'Accordion', path: '/accordion'},
-        { label: 'Button', path: '/button'}
-
-    ]
+      { label: "Dropdown", path: "/" },
+      { label: "Accordion", path: "/accordion" },
+      { label: "Button", path: "/button" },
+      { label: "Modal", path: "/modal" },
+    ];
 
    const renderedLinks = links.map((link) => (
     <Link key={link.label} to={link.path} className="mb-3" activeClassName={"font-bold border-l-4 border-blue-500 pl-2"}>
